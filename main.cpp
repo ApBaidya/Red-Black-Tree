0e25b12dbd730807ef3f0f1429506fbed4d5acd5
ApBaidya <aparajitabaidya2@gmail.com>
2026-04-09 05:10:50 +0100

    first step to greatness

---
+++ main.cpp
@@ -0,0 +1,102 @@
+/*
+Aparajita Baidya 4.8.2026
+Red Black Tree
+
+Insertion due 4.24
+-add one number or file input --> update tree
+-print
+
+Deletion due 5.15
+-search
+-remove --> update tree
+*/
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+//function defs
+void ADD();
+void fileADD();
+void Display();
+void Search();
+void Remove();
+void Quit();
+
+//node struct, haven't used em in a bit
+struct Node{
+  int data;
+  string color;
+  Node* right;
+  Node* left;
+  Node* parent;
+};
+
+int main(){
+  //some variables
+  char input[2];//user input
+  int running = 1;
+  //root
+  Node* root = nullptr;
+  while(running){
+    cout<<"[a] add one number, [f] file add, [d] display, [s] search, [r] remove, [q] quit"<<endl;
+    cin>>input;
+    cin.ignore(10, '\n');
+    cin.clear();
+    if(strcmp(input,"a")==0){
+      //single input
+      cout<<"done"<<endl;
+    }
+    if(strcmp(input,"f")==0){
+      //file
+      cout<<"done"<<endl;
+    }
+    if(strcmp(input,"d")==0){
+      //display
+      cout<<"done"<<endl;
+    }
+    if(strcmp(input,"s")==0){
+      //search
+      cout<<"done"<<endl;
+    }
+    if(strcmp(input,"r")==0){
+      //remove
+      cout<<"done"<<endl;
+    }
+    if(strcmp(input,"q")==0){
+      //quit
+      running = 0;
+    }
+  }
+  cout<<"Bye!"<<endl;
+  return 0;
+}
+
+//add
+void ADD(){
+
+}
+
+//file add
+void fileADD(){
+
+}
+
+//display
+void Display(){
+
+}
+
+//search
+void Search(){
+
+}
+//remove
+
+//quit
+void Quit(){
+
+}
